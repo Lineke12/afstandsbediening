275e661422eb7f14b49ba3191248249e0f301618
Lineke12 <151532516+Lineke12@users.noreply.github.com>
2023-12-04 12:50:04 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let ir_waarde = 0
+let vooruuit = 1
+let achteruit = 2
+let links = 3
+let rechts = 4
+basic.forever(function () {
+    let stop = 0
+    if (ir_waarde == vooruuit) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == achteruit) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == links) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == rechts) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (ir_waarde == stop) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+    basic.showNumber(ir_waarde)
+})
